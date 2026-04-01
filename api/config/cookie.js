1fc03777ba198d6bcf714c99b2d8163f7d7481f7
Paeru <153697082+paeru12@users.noreply.github.com>
2026-04-01 17:04:46 +0700

    fix cookie

--- api/config/cookie.js
+++ api/config/cookie.js
@@ -1,69 +1,52 @@
+const isProd =
+  process.env.NODE_ENV === "production"
+
 module.exports = {
+
   accessCookie: {
+
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
-    maxAge: 3 * 60 * 1000,
+
+    secure: isProd,
+
+    sameSite:
+      isProd
+        ? "none"
+        : "lax",
+
+    domain:
+      isProd
+        ? ".belisenang.id"
+        : undefined,
+
     path: "/",
+
+    maxAge:
+      15 * 60 * 1000
+
   },
+
   refreshCookie: {
+
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+
+    secure: isProd,
+
+    sameSite:
+      isProd
+        ? "none"
+        : "lax",
+
+    domain:
+      isProd
+        ? ".belisenang.id"
+        : undefined,
+
     path: "/",
-  },
+
+    maxAge:
+      7 * 24 * 60 * 60 * 1000
+
+  }
+
 };
-
-// const isProd =
-//   process.env.NODE_ENV === "production"
-
-// module.exports = {
-
-//   accessCookie: {
-
-//     httpOnly: true,
-
-//     secure: isProd,
-
-//     sameSite:
-//       isProd
-//         ? "none"
-//         : "lax",
-
-//     domain:
-//       isProd
-//         ? ".belisenang.id"
-//         : undefined,
-
-//     path: "/",
-
-//     maxAge:
-//       15 * 60 * 1000
-
-//   },
-
-//   refreshCookie: {
-
-//     httpOnly: true,
-
-//     secure: isProd,
-
-//     sameSite:
-//       isProd
-//         ? "none"
-//         : "lax",
-
-//     domain:
-//       isProd
-//         ? ".belisenang.id"
-//         : undefined,
-
-//     path: "/",
-
-//     maxAge:
-//       7 * 24 * 60 * 60 * 1000
-
-//   }
-
-// };
