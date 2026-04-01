943d2af6de6b14ba0be96613a948ea23ba5ccc86
Paeru <153697082+paeru12@users.noreply.github.com>
2026-04-01 18:32:52 +0700

    tx rate route

--- api/routes/dash/setting.route.js
+++ api/routes/dash/setting.route.js
@@ -6,10 +6,10 @@ const router = express.Router();
 const { creatorGuard } = require("../../middlewares/guard/role.guard");
 const { autoFilterByCreator } = require("../../middlewares/autoFilterCreator.middleware");
 // Get tax rate
-router.get("/tax-rate", controller.getTaxRate);
+router.get("/tax-rate",creatorGuard("SUPER_ADMIN", "SYSTEM_ADMIN","PROMOTOR_OWNER","PROMOTOR_EVENT_ADMIN"), controller.getTaxRate);
 router.get("/admin-fee", creatorGuard("PROMOTOR_OWNER", "PROMOTOR_EVENT_ADMIN"), autoFilterByCreator(), controller.getAdminFee);
 
 // Update tax rate
-router.put("/tax-rate", controller.updateTaxRate);
-
+router.put("/tax-rate", creatorGuard("SUPER_ADMIN", "SYSTEM_ADMIN"), controller.updateTaxRate);
+ 
 module.exports = router;
